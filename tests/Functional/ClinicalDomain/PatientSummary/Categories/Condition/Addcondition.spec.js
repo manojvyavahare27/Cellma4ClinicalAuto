2bc52bb8661e14ec3c4efb2b548c5398d5619168
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-07-29 11:20:37 +0530

    Add condition

--- tests/Functional/ClinicalDomain/PatientSummary/Categories/Condition/Addcondition.spec.js
+++ tests/Functional/ClinicalDomain/PatientSummary/Categories/Condition/Addcondition.spec.js
@@ -108,13 +108,13 @@ test.describe("condition Category", () => {
       await page.waitForTimeout(1500);
       await confirmexisting.clickOnConfirmExistingDetails();   
 
-      await page.waitForTimeout(5000);
-      const alertPopup= await page.locator("xpath=//h2[text()='Alerts']").isVisible()      
-      if(alertPopup==true)
-        {       
-          await condition.closePopUp()
-        }
-      await page.waitForTimeout(2000);
+      // await page.waitForTimeout(5000);
+      // const alertPopup= await page.locator("xpath=//h2[text()='Alerts']").isVisible()      
+      // if(alertPopup==true)
+      //   {       
+      //     await condition.closePopUp()
+      //   }
+      // await page.waitForTimeout(2000);
       
        await contacthistory.clickOnShowFilter()  
       await contacthistory.selectServiceFilter("General Medicine Automation");
@@ -122,7 +122,6 @@ test.describe("condition Category", () => {
       await contacthistory.selectContactReason("Assessments");
       await contacthistory.selectContactLocation("Cardio Location");     
     
-      await page.pause()
       await contacthistory.clickOnAddContact();      
        await condition.selectCategoryFromList(jsonData.AddCondition[index].pacr_category);
        await page.waitForTimeout(2000)       
@@ -161,8 +160,6 @@ test.describe("condition Category", () => {
       const patId = results[0].paa_pat_id;
       console.log("Patient Accessed by User:" + patId);
 
-     await page.pause()
-
 ////////ADD NEW condition/////
 
  await page.waitForTimeout(2000);
@@ -190,11 +187,25 @@ test.describe("condition Category", () => {
       await page.pause()
       console.log("My Pat id is:"+patId);
       
-sqlQuery="select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_risk, "+
-" cond_date_diagnosed, cond_notes  from patient_clinical_records join patient_clinical_records_details "+
-"on pacr_id=pacrd_pacr_id join conditions on pacr_id=cond_pacr_id where pacr_record_status='approved' and "+
-" pacr_pat_id=" + patId + " and pacrd_record_status='approved' and cond_record_status='approved' and "+
-"pacr_que_name='" + jsonData.Addcondition[index].pacr_que_name + "' and pacr_category='conditions' order by 1 desc limit 1";
+      sqlQuery=
+       "select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_risk, cond_date_diagnosed, cond_notes "+
+       " from patient_clinical_records join patient_clinical_records_details "+
+       " on pacr_id=pacrd_pacr_id join conditions on pacr_id=cond_pacr_id where pacr_record_status='approved' and "+
+       " pacr_pat_id=789847 and pacrd_record_status='approved' and cond_record_status='approved' and "+
+       "pacr_que_name='Erythematous condition' and pacr_category='condition' order by 1 desc limit 1"
+
+
+
+
+
+
+
+
+// sqlQuery="select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_risk, "+
+// " cond_date_diagnosed, cond_notes  from patient_clinical_records join patient_clinical_records_details "+
+// "on pacr_id=pacrd_pacr_id join conditions on pacr_id=cond_pacr_id where pacr_record_status='approved' and "+
+// " pacr_pat_id=" + patId + " and pacrd_record_status='approved' and cond_record_status='approved' and "+
+// "pacr_que_name='" + jsonData.Addcondition[index].pacr_que_name + "' and pacr_category='condition' order by 1 desc limit 1";
 
     console.log(sqlQuery)
       //select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_risk, diag_date_onset, diag_date_firstseen, diag_date_diagnosed, diag_notes from patient_clinical_records join patient_clinical_records_details on pacr_id=pacrd_pacr_id join diagnosis on pacr_id=diag_pacr_id where pacr_record_status='approved' and pacrd_record_status='approved' and diag_record_status='approved' and pacr_pat_id='787755' and pacr_record_status='approved' and pacr_que_name='Dengue haemorrhagic fever' and pacr_category='diagnosis' order by 1 desc limit 1;
@@ -220,39 +231,149 @@ sqlQuery="select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_r
       await condition.clickOnItemEdit();
       //await page.locator("xpath=//div[@data-testid='allergies']").click()
       await page.waitForTimeout(500);
-      await page.locator("xpath=//button[@data-testid='episodes[0].id']").click()
-      await page.waitForTimeout(500);
-      await page.pause()
-     // await conditionExtraDetails.clickOnclinicalSubcategorycondition();
-      await conditionExtraDetails.clickOnSpecificAllergyName()           
-      await page.getByRole('checkbox', { name: 'Private record' }).click()   
-
-     // await allergyExtraDetails.selectFrequency("1");
      
-     await conditionExtraDetails.enterAllergyStartDate(jsonData.EditCondition[index].alrg_start_date)
-     await conditionExtraDetails.enterconditionEndDate(jsonData.EditCondition[index].alrg_end_date)
-     //await conditionExtraDetails.selectReaction(jsonData.Editcondition[index].eli_text)
-     //await conditionExtraDetails.selectReactionSevirity(jsonData.Editcondition[index].alrg_reaction_severity)
-     await conditionExtraDetails.enterallergyTextArea(jsonData.EditCondition[index].alrg_notes)
+//      // await conditionExtraDetails.clickOnclinicalSubcategorycondition();
+//       await conditionExtraDetails.clickOnSpecificAllergyName()           
+//       await page.getByRole('checkbox', { name: 'Private record' }).click()   
+
+//      // await allergyExtraDetails.selectFrequency("1");
+     
+//      await conditionExtraDetails.enterAllergyStartDate(jsonData.EditCondition[index].alrg_start_date)
+//      await conditionExtraDetails.enterconditionEndDate(jsonData.EditCondition[index].alrg_end_date)
+//      //await conditionExtraDetails.selectReaction(jsonData.Editcondition[index].eli_text)
+//      //await conditionExtraDetails.selectReactionSevirity(jsonData.Editcondition[index].alrg_reaction_severity)
+//      await conditionExtraDetails.enterallergyTextArea(jsonData.EditCondition[index].alrg_notes)
+
+//       //await allergyExtraDetails.enterClinicalItemNotes("Updated Allergy Notes From Playwright");
+     
+//       //await page.locator("xpath=//button[@aria-label='saveExtraDetails']").click()
+//       await conditionExtraDetails.clickOnSaveExtraDetails();
+//       await page.getByLabel('saveChecklist').click()
+//       await page.waitForTimeout(1000);
+//       await expect(page.getByText('condition record updated successfully')).toHaveText('condition record updated successfully')
+      
+//        ////// Database comparison - Patient Clinical Records - UPDATE condition/////////
+//      sqlQuery =
+//      "select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_risk,  alrg_notes"+
+//      " from patient_clinical_records join patient_clinical_records_details"+
+//      " on pacr_id=pacrd_pacr_id join allergies on pacr_id=alrg_pacr_id where pacr_record_status='approved'"+
+//      " and pacrd_record_status='approved' and alrg_record_status='approved' and pacr_id=" + pacrId +
+//      " and pacr_record_status='approved'";
+          
+//      console.log("Manoj:"+sqlQuery);
+//    sqlFilePath = "SQLResults/ClinicalDomain/patientClinicalRecord.json";
+//    results = await executeQuery(sqlQuery, sqlFilePath);
+
+//    console.log("\n Patient Clinical Records stored into the database: \n", results);
+//    var match = await compareJsons(sqlFilePath, null, jsonData.EditCondition[index]);
+//    if (match) {
+//      console.log(
+//        "\n Update Patient Clinical Records Comparision Edit condition: Parameters from both JSON files match!\n"
+//      );
+//    } else {
+//      console.log(
+//        "\n Update Patient Clinical Records Comparision Edit condition: Parameters from both JSON files do not match!\n"
+//      );
+//    }
+
+  
+//    ////////AUTO UPDATE RISK AFTER UPDATING OUTCOME /////
+      
+//       await condition.clickOnItemHighlightNone();
+//       await page.waitForTimeout(500);
+//       await condition.selectLowRiskLevel();
+//       await page.waitForTimeout(500);
+//       await condition.selectModerateRiskLevel();
+//       await page.waitForTimeout(500);
+//       await condition.selectHighRiskLevel();
+//       await page.waitForTimeout(500);
+//       await condition.selectAllRiskLevel();
+//       await condition.clickOnLevelTwoExtraDetails();
+//       await condition.clickOnLevelThreeExtraDetails();
+//       await condition.clickOnLevelOneExtraDetails();
+
+
+//       ////// Database comparison - Patient Clinical Records - UPDATE condition RISK/////////
+//      sqlQuery =
+//      "select pacr_risk from patient_clinical_records where pacr_id=" + pacrId;
+          
+//    sqlFilePath = "SQLResults/ClinicalDomain/patientClinicalRecord.json";
+//    results = await executeQuery(sqlQuery, sqlFilePath);
+//    if(results[0].pacr_risk == jsonData.EditCondition[index].pacr_risk){
+//     console.log(
+//       "\n Patient Clinical Records Comparision for Edit condition Risk: RISK Updated Successfully! \n"
+//     );
+//    } else {
+//     console.log(
+//       "\n Patient Clinical Records Comparision for Edit condition Risk: RISK Update Failed! \n"
+//     );
+//   }
+
+//      ///////// Deleting Item ////////////
+
+      
+//       await condition.clickOnItemEdit();
+//       await conditionExtraDetails.clickOnDelete();
+//       await conditionExtraDetails.clickOnCancelDelete();
+//       await conditionExtraDetails.clickOnDelete();
+//       await conditionExtraDetails.clickOnConfirmDelete();
+//       await conditionExtraDetails.enterDeleteReason(jsonData.DeleteCondition[index].pacr_delete_reason);
+//       await conditionExtraDetails.clickOnSaveDeleteReason();
+//       await page.waitForTimeout(1000)
+    
+
+//        ////// Database comparison- Patient Clinical Records - DELETE OUTCOME/////////
+//        sqlQuery ="select pacr_que_name, pacr_delete_reason from patient_clinical_records where pacr_id=" +
+//        pacrId +
+//        " and pacr_record_status='wrong'";
+
+//      sqlFilePath = "SQLResults/ClinicalDomain/patientClinicalRecord.json";
+//      results = await executeQuery(sqlQuery, sqlFilePath);
+//      //  pacrId=results[0].pacr_id;
+//      console.log("\n Patient Details stored into the database: \n", results);
+//      var match = await compareJsons(
+//        sqlFilePath,
+//        null,
+//        jsonData.DeleteCondition[index]
+//      );
+//      if (match) {
+//        console.log(
+//          "\n  Patient Clinical Records Comparision for Delete condition: Parameters from both JSON files match!\n"
+//        );
+//      } else {
+//        console.log(
+//          "\n  Patient Clinical Records Comparision: Parameters from both JSON files do not match!\n"
+//        );
+//      }       
+     
+//     }
+//   });
+// });
+//await page.locator("xpath=//div[@data-testid='allergies']").click()
+      
+     
+     
+    await conditionExtraDetails.enterCoditionNotes(jsonData.EditCondition[0].cond_notes);
+    // await conditionExtraDetails.enterallergyTextArea(jsonData.EditCondition[index].alrg_notes)
 
       //await allergyExtraDetails.enterClinicalItemNotes("Updated Allergy Notes From Playwright");
      
       //await page.locator("xpath=//button[@aria-label='saveExtraDetails']").click()
       await conditionExtraDetails.clickOnSaveExtraDetails();
-      await page.getByLabel('saveChecklist').click()
-      await page.waitForTimeout(1000);
-      await expect(page.getByText('condition record updated successfully')).toHaveText('condition record updated successfully')
+      
+      //await expect(page.getByText('condition record updated successfully')).toHaveText('condition record updated successfully')
       
        ////// Database comparison - Patient Clinical Records - UPDATE condition/////////
-     sqlQuery =
-     "select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_risk,  alrg_notes"+
-     " from patient_clinical_records join patient_clinical_records_details"+
-     " on pacr_id=pacrd_pacr_id join allergies on pacr_id=alrg_pacr_id where pacr_record_status='approved'"+
-     " and pacrd_record_status='approved' and alrg_record_status='approved' and pacr_id=" + pacrId +
-     " and pacr_record_status='approved'";
+    sqlQuery =
+      "select pacr_id, pacr_category, pacr_que_name, cond_date_diagnosed, cond_notes from patient_clinical_records "
+      +" join patient_clinical_records_details on pacr_id=pacrd_pacr_id join conditions on pacr_id=cond_pacr_id "
+      +"where pacr_record_status='approved' "
+      +"and pacr_pat_id= " + patId +" "
+      +"and pacr_record_status='approved' and pacr_que_name='"+jsonData.AddCondition[index].pacr_que_name +"' "
+      +"and pacr_category='condition' order by 1 desc limit 1;"
           
      console.log("Manoj:"+sqlQuery);
-   sqlFilePath = "SQLResults/ClinicalDomain/patientClinicalRecord.json";
+   sqlFilePath = "SQLResults/ClinicalDomain/AddCondition.json";
    results = await executeQuery(sqlQuery, sqlFilePath);
 
    console.log("\n Patient Clinical Records stored into the database: \n", results);
@@ -288,7 +409,7 @@ sqlQuery="select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_r
      sqlQuery =
      "select pacr_risk from patient_clinical_records where pacr_id=" + pacrId;
           
-   sqlFilePath = "SQLResults/ClinicalDomain/patientClinicalRecord.json";
+   sqlFilePath = "SQLResults/ClinicalDomain/AddCondition.json";
    results = await executeQuery(sqlQuery, sqlFilePath);
    if(results[0].pacr_risk == jsonData.EditCondition[index].pacr_risk){
     console.log(
@@ -310,15 +431,15 @@ sqlQuery="select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_r
       await conditionExtraDetails.clickOnConfirmDelete();
       await conditionExtraDetails.enterDeleteReason(jsonData.DeleteCondition[index].pacr_delete_reason);
       await conditionExtraDetails.clickOnSaveDeleteReason();
-      await page.waitForTimeout(1000)
-    
+await expect(page.getByText("Condition deleted successfully")).toHaveText("Condition deleted successfully");
+          
+      await page.waitForTimeout(1000)   
 
        ////// Database comparison- Patient Clinical Records - DELETE OUTCOME/////////
        sqlQuery ="select pacr_que_name, pacr_delete_reason from patient_clinical_records where pacr_id=" +
-       pacrId +
-       " and pacr_record_status='wrong'";
+       pacrId +" and pacr_record_status='wrong'";
 
-     sqlFilePath = "SQLResults/ClinicalDomain/patientClinicalRecord.json";
+     sqlFilePath = "SQLResults/ClinicalDomain/AddCondition.json";
      results = await executeQuery(sqlQuery, sqlFilePath);
      //  pacrId=results[0].pacr_id;
      console.log("\n Patient Details stored into the database: \n", results);
