21a4f574a90d8e2f70701373fe58778686fc857a
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-10-31 18:24:32 +0530

    change interpretation to small

--- tests/Functional/ClinicalDomain/PatientSummary/Categories/Interpretations/AddInterpretation.spec.js
+++ tests/Functional/ClinicalDomain/PatientSummary/Categories/Interpretations/AddInterpretation.spec.js
@@ -172,7 +172,7 @@ test.describe("Interpretations Category", () => {
       ////// Database comparison- Patient Clinical Records - ADDING NEW Interpretations/////////
       sqlQuery =
       "select pacr_id, pacr_category, pacr_que_name, pacr_clinic_date, pacr_risk,inte_outcome_eli_text, inte_notes"+
-      " from patient_clinical_records join patient_clinical_records_details on pacr_id=pacrd_pacr_id join Interpretations on pacr_id=inte_pacr_id where pacr_record_status='approved'"+
+      " from patient_clinical_records join patient_clinical_records_details on pacr_id=pacrd_pacr_id join interpretations on pacr_id=inte_pacr_id where pacr_record_status='approved'"+
       " and pacr_pat_id=" + patId +
       " and pacr_record_status='approved' and pacr_que_name='" + jsonData.AddInterpretation[index].pacr_que_name +
       "' and pacr_category='interpretation' order by 1 desc limit 1";
